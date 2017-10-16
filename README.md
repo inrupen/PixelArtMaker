0de6bf26f8df19401c7d2c22f87ebbd60bda3627
inrupen <inrupen@gmail.com>
2017-10-15 22:48:26 -0500

    Code indented, Final code corrcted

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Pixel Art Maker Project
 
-Select a grid-layout - height and width.
-Select a color and use the mouse-click to draw.
+Program aims to create a grid layout N*M , and color the grid.
+
+#Step-by-step instructions could be:
+
+1. Open index.html in a modern web browser.
+2. Choose the dimensions of your grid by entering integer values between 1 and 48 inclusive in the height and width input boxes.
+3. Click submit to create the grid.
+4. Choose a color by picking a color via colorPicker or use default-black.
+5. Color the grid by clicking over the boxes.
--- js/designs.js
+++ js/designs.js
@@ -1,49 +1,31 @@
+const colorP = document.getElementById("colorPicker");  //get color for grid
+
+//event listener on "submit"
+const sizePicker = document.querySelector("#sizePicker");
+sizePicker.addEventListener("submit", function(e) {
+  e.preventDefault();
+  makeGrid();
+});
+
+
 function makeGrid() {
 
-// getting inputs-height & weight
+  //getting inputs-height & weight
   let height = $("#input_height").val();
-
   let width = $("#input_width").val();
 
   //clear the canvas before creating new grid-layout
   $("#pixel_canvas").empty();
-  if(height>48 || width>48){
-    alert("Select value from 1 to 48");
-  }
-  else{
-    //make grid
-    for(let i=1;i<=height;i++){       //height
-      $("#pixel_canvas").append("<div class='row'> </div>");
-      for(let j=1;j<=width;j++){       //width
-        $(".row:last").append("<div class='rectangle'> </div>");
-      }
+
+  for(let i=1;i<=height;i++){       //height
+    $("#pixel_canvas").append("<div class='row'> </div>");
+    for(let j=1;j<=width;j++){       //width
+      $(".row:last").append("<div class='rectangle'> </div>");
     }
   }
-}
-
-
-// const table = document.getElementById('table_id');
-// for (let i = 0; i < 10; i++) {
-//     // Inserts 10 rows into the table
-//     const row = table.insertRow(i);
-//     for (let j = 0; j < 10; j++) {
-//         // Inserts 10 cells into each of the rows
-//         const cell = row.insertCell(j);
-//     }
-// }
-
-
-  function colorGrid() {
-  const colorP = document.getElementById("colorPicker");  //get color for grid
 
   //fill grid with color-with clickedon
   $(".rectangle").click(function() {
     $( this ).css( "background-color",colorP.value);
   });
-
-  //fill grid with color-with hoveron
-  // $(".rectangle").hover(function() {
-  //     $( this ).css( "background-color",colorP );
-  //   });
-
 }
